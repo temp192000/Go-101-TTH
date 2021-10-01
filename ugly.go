296f61cacd3b51e204bb793ecd046487804cf23a
temp192000 <codemonkey.ranjith@gmail.com>
2021-10-01 14:39:40 +0530

    gofmt: code refactor

---
+++ ugly.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var myNumber int
+	var myString string
+	var myFlag bool
+
+	fmt.Println(myNumber) // Default: 0
+	fmt.Println(myString) // Default: ""
+	fmt.Println(myFlag)   // Default: false
+}
